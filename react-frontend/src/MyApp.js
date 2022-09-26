32b79ec5ab15ea9cfc482aa1f73387a3aaf0873f
clparadi@calpoly.edu <clparadi@calpoly.edu>
2022-09-26 12:06:39 -0700

    fixed linking

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
-import Table from './Table'
+import Table from './Table';
+import Form from './Form';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
