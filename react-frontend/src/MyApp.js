df5230608f3d52c267e2b86a4d72d7f729518000
clparadi@calpoly.edu <clparadi@calpoly.edu>
2022-09-26 11:08:13 -0700

    added Form

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -2,25 +2,25 @@ import React, {useState} from 'react';
 import Table from './Table'
 
 function MyApp() {
-    const [characters, setCharacters] = useState([
-        {
-          name: 'Charlie',
-          job: 'Janitor',
-           // the rest of the data
-        },
-        {
-          name: 'Mac',
-          job: 'Bouncer',
-        },
-        {
-          name: 'Dee',
-          job: 'Aspring actress',
-        },
-        {
-          name: 'Dennis',
-          job: 'Bartender',
-        },
-      ]);  
+    const [characters, setCharacters] = useState([]);
+    //     {
+    //       name: 'Charlie',
+    //       job: 'Janitor',
+    //        // the rest of the data
+    //     },
+    //     {
+    //       name: 'Mac',
+    //       job: 'Bouncer',
+    //     },
+    //     {
+    //       name: 'Dee',
+    //       job: 'Aspring actress',
+    //     },
+    //     {
+    //       name: 'Dennis',
+    //       job: 'Bartender',
+    //     },
+    //   ]);  
 
 function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
