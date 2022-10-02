f67255e45ed0b261b9840bc67b07ca2770656d13
camilleparadis <67288349+camilleparadis@users.noreply.github.com>
2022-10-02 15:45:43 -0700

    Add files via upload
    trying to fix branching issues

---
+++ MyApp.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect} from 'react';
+import Table from './Table';
+import Form from './Form';
+import axios from 'axios';
+
+function MyApp() {
+    const [characters, setCharacters] = useState([]);
+  
+function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+    }
+
+// async function deleteCharacterAx(person){
+//     try {
+//        deleteId = person['id'];
+//        const response = await axios.delete('http://localhost:5001/users', {id = deleteId});
+//        return response;
+//     }
+//     catch (error) {
+//        console.log(error);
+//        return false;
+//     }
+// } 
+
+function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 201)
+    // is this the correct way to implement repsonse.data?
+        person = result.data;
+        setCharacters([...characters, person] );
+    });
+}
+
+async function fetchAll(){
+    try {
+        const response = await axios.get('http://localhost:5001/users');
+        return response.data.users_list;     
+    }
+    catch (error){
+        //We're not handling errors. Just logging into the console.
+        console.log(error); 
+        return false;         
+    }
+    }
+
+useEffect(() => {
+    fetchAll().then( result => {
+        if (result)
+            setCharacters(result);
+    });
+}, [] );
+
+async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5001/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+}
+
+return (
+    <div className="container">
+        <Table characterData={characters} removeCharacter={removeOneCharacter} />
+        <Form handleSubmit={updateList} />
+    </div>
+    )
+}
+export default MyApp;
