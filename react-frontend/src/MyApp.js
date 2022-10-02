b49c8839852e7c79e4db60258227e2d1aff47e88
clparadi@calpoly.edu <clparadi@calpoly.edu>
2022-10-02 13:30:07 -0700

    updated fetch n linked

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,28 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
+import axios from 'axios';
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
-    //     {
-    //       name: 'Charlie',
-    //       job: 'Janitor',
-    //        // the rest of the data
-    //     },
-    //     {
-    //       name: 'Mac',
-    //       job: 'Bouncer',
-    //     },
-    //     {
-    //       name: 'Dee',
-    //       job: 'Aspring actress',
-    //     },
-    //     {
-    //       name: 'Dennis',
-    //       job: 'Bartender',
-    //     },
-    //   ]);  
-
+  
 function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
         return i !== index
@@ -30,9 +13,43 @@ function removeOneCharacter (index) {
       setCharacters(updated);
     }
 
-function updateList(person) {
-    setCharacters([...characters, person]);
+function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result && result.status === 201)
+        setCharacters([...characters, person] );
+    });
+}
+
+async function fetchAll(){
+    try {
+        const response = await axios.get('http://localhost:5001/users');
+        return response.data.users_list;     
     }
+    catch (error){
+        //We're not handling errors. Just logging into the console.
+        console.log(error); 
+        return false;         
+    }
+    }
+
+useEffect(() => {
+    fetchAll().then( result => {
+        if (result)
+            setCharacters(result);
+    });
+}, [] );
+
+async function makePostCall(person){
+    try {
+       const response = await axios.post('http://localhost:5001/users', person);
+       return response;
+    }
+    catch (error) {
+       console.log(error);
+       return false;
+    }
+}
+
 return (
     <div className="container">
         <Table characterData={characters} removeCharacter={removeOneCharacter} />
